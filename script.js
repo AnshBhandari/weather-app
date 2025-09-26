274ca1f54fe0b1822833c895768e51abab652e3f
Ansh Bhandari <91427623+AnshBhandari@users.noreply.github.com>
2025-09-26 22:23:59 +0530

    added empty city name handler

--- script.js
+++ script.js
@@ -11,29 +11,32 @@ const searchButton = document.querySelector(".search button");
 const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    let data = await response.json();
-    if (response.status === 404) {
+    if (city.trim() === "") {
         document.querySelector(".error").style.display = "block";
+        document.querySelector(".error").querySelector("p").textContent = "city name can't be empty!";
         document.querySelector(".weather").style.display = "none";
-    }
-    else {
-        // console.log(data);
+    } else {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        let data = await response.json();
+        if (response.status === 404) {
+            document.querySelector(".error").style.display = "block";
+            document.querySelector(".weather").style.display = "none";
+        } else {
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+            document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
 
-        document.querySelector(".city").innerHTML = data.name;
-        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+            if (data.weather[0].main === "Clouds") weatherIcon.src = "images/clouds.png";
+            else if (data.weather[0].main === "Clear") weatherIcon.src = "images/clear.png";
+            else if (data.weather[0].main === "Rain") weatherIcon.src = "images/rain.png";
+            else if (data.weather[0].main === "Drizzle") weatherIcon.src = "images/drizzle.png";
+            else if (data.weather[0].main === "Mist") weatherIcon.src = "images/mist.png";
+            else if (data.weather[0].main === "Haze") weatherIcon.src = "images/haze.png";
 
-        if (data.weather[0].main === "Clouds") weatherIcon.src = "images/clouds.png";
-        else if (data.weather[0].main === "Clear") weatherIcon.src = "images/clear.png";
-        else if (data.weather[0].main === "Rain") weatherIcon.src = "images/rain.png";
-        else if (data.weather[0].main === "Drizzle") weatherIcon.src = "images/drizzle.png";
-        else if (data.weather[0].main === "Mist") weatherIcon.src = "images/mist.png";
-        else if (data.weather[0].main === "Haze") weatherIcon.src = "images/haze.png";
-
-        document.querySelector(".weather").style.display = "block";
-        document.querySelector(".error").style.display = "none";
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display = "none";
+        }
     }
 }
 
@@ -46,4 +49,5 @@ searchBox.addEventListener("keypress", (event) => {
 searchButton.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
 })
+
 
